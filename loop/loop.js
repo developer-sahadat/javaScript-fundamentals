ef05f702e623aeb02b6cff18caa25003dd037756
developer-sahadat <mdridoysikder862@gmail.com>
2022-03-12 16:05:50 +0600

    loop

---
+++ loop/loop.js
@@ -0,0 +1,12 @@
+/* Two basic loops, for the loop you must know. You can also see the while loop. Although we will use less of these two loops. If you ever feel like that, you can understand. */
+
+for(let i=1; i<=10; i++){
+    console.log(i);
+}
+
+let x=1;
+
+while(x<=10){
+console.log(x);
+x++
+}
