7d60d35a676d24f54a9c46f4ea002e407b733375
developer-sahadat <mdridoysikder862@gmail.com>
2022-03-12 22:04:39 +0600

    default

---
+++ es6/default-parameters/default.js
@@ -0,0 +1,8 @@
+const programming=(first, second, third='please enter')=>{
+  const developer=[first, second, third]
+  return developer
+}
+
+
+const result=programming('html', 'css', );
+console.log(result);
