c9af5258d5a3e8f0e41b8fb3e5543e7ef609e494
developer-sahadat <mdridoysikder862@gmail.com>
2022-03-12 21:58:55 +0600

    shorthand

---
+++ es6/shorthand/shorthand.js
@@ -0,0 +1,13 @@
+let name='Md Shahadat Hossain';
+let address='Dhaka Bangladesh';
+let id=572;
+let subject='Science';
+
+const student={
+    name,
+    address,
+    id,
+    subject
+}
+
+console.log(student.name);
