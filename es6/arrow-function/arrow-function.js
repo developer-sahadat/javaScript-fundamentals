dac88ea472f68ab39877a9741e3fb887a509fce0
developer-sahadat <mdridoysikder862@gmail.com>
2022-03-12 21:35:09 +0600

    arrow function

---
+++ es6/arrow-function/arrow-function.js
@@ -0,0 +1,13 @@
+const nineNumber=()=> 9;
+console.log(nineNumber());
+
+const multiplication=(num)=>num*12;
+
+console.log(multiplication(20));
+
+const divition=(num1, num2)=>{
+        const sum= num1+num2
+        return sum/4
+    };  
+
+console.log(divition(20, 20));
