33c2995b19145c460a4ee1288411beec0df7f071
developer-sahadat <mdridoysikder862@gmail.com>
2022-03-12 21:53:50 +0600

    destructuring

---
+++ es6/destructuring/destructuring.js
@@ -0,0 +1,19 @@
+const programming={
+    mySkils:'Mern Stact Developer',
+    address:'Dhaka',
+    favorite:{
+        color:'white',
+
+    }
+}
+
+
+const {mySkils}=programming;
+
+console.log(mySkils);
+
+
+const development=['HTML', 'CSS', 'JavaScript']
+
+const [css]=development;
+console.log(css);
